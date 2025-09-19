311b002a912b0ea748e76faadd5b108acd2ca97b
Daria Kulikova <dariakulikova01@gmail.com>
2025-09-19 14:52:22 +0300

    update gendiff.py, add format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,10 +4,16 @@ import argparse
 def make_parser():
     parser = argparse.ArgumentParser(
         prog='gendiff',
-        description='Compares two configuration files and shows a difference.'
-        )
+        description='Compares two configuration files and shows a difference.',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        metavar='FORMAT',
+        help='set format of output'
+    )
     return parser
 
 def main():
